96e03c71902104d312415587004d15a7f1fffc5d
Aliaksei <alxqwerty@gmail.com>
2022-09-27 15:57:27 +0300

    Refreshed knowledge about basic loops.

--- js/script.js
+++ js/script.js
@@ -1,34 +1,24 @@
 "use strict";
 
-if (4 == 9) {
-    console.log('Ok!');
-} else {
-    console.log('Not equal.');
+let num = 50;
+
+while (num <= 55) {
+    console.log(num);
+    num++;
 }
 
+do {
+    console.log(num);
+    num--;
+} while (num >= 45);
 
-// if (num < 49) {
-//     console.log('Error');
-// } else if (num > 100) {
-//     console.log('Too much');
-// } else {
-//     console.log('Correct!');
-// }
+for (let i = 1; i < 10; i++) {
+    // if (num == 53) {
+    //     break;
+    // }
+    if (i === 2) {
+       continue;
+    }
 
-// (num === 50) ? console.log('OK!') : console.log('Error!');
-
-const num = 50;
-
-switch (num) {
-    case 49: 
-        console.log('Error.');
-        break;
-    case 100:
-        console.log('One hundred.');
-        break;
-    case 50:
-        console.log('Correct!');
-        break;
-    default:
-        console.log('Not this time.');
+    console.log(i);
 }

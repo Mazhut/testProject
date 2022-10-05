792153c699ee6a1a8cca2c39e679aa6bce9d2e80
Aliaksei <alxqwerty@gmail.com>
2022-10-05 22:53:24 +0300

    Scryptonite

--- js/script.js
+++ js/script.js
@@ -1,30 +1,68 @@
 "use strict";
 
-function first() {
-    //Do something
-    setTimeout(function(){
-        console.log(1);
-    }, 500);
-}
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        background: 'red'
+    },
+    makeTest: function() {
+        console.log("Test");
+    }
+};
 
-function second() {
-    console.log(2);
-}
+options.makeTest();
 
-// first();
-// second();
+const {border, background} = options.colors;
+console.log(border);
+console.log(background);
 
-function learnJS(lang, callback) {
-    console.log(`I am learning: ${lang}`);
-    callback();
-}
+// console.log(Object.keys(options).length);
 
-function done() {
-    console.log('I have finished this task.');
-}
+// const playerInfo = {
+//     firstName: 'Charles',
+//     lastName: 'DeKetelaare',
+//     jersey: 90,
+//     contractInfo: {
+//         salary: "$2.500.000",
+//         expiryDate: "30.06.2027"
+//     },
+//     age: 21
+// };
 
-learnJS('JavaScript', function() {
-    console.log('I have finished this work.');
-});
+// console.log(Object.keys(playerInfo).length);
 
-learnJS('JavaScript', done);
+// for (let key in playerInfo) {
+//     if (typeof(playerInfo[key]) === 'object') {
+//         for (let i in playerInfo[key]) {
+//             console.log(`Parameter ${i}: ${playerInfo[key][i]}`);
+//         }
+//     } else {
+//         console.log(`Parameter ${key}: ${playerInfo[key]}`);
+//     }
+// }
+
+
+// console.log(options.name);
+
+// delete options.name;
+
+// console.log(options);
+
+// let counter = 0;
+
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object') {
+//         for (let i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//             counter++;
+//         }
+//     }else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//         counter++;
+//     }
+// }
+
+// console.log(counter);

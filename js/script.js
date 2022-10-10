82fef4abd8716c5a3afa9b438682dc6dfab2c0bf
Aliaksei <alxqwerty@gmail.com>
2022-10-10 20:24:42 +0300

    Dynamic typing

--- js/script.js
+++ js/script.js
@@ -1,23 +1,54 @@
 "use strict";
 
-function hello() {
-    console.log('Hello world!');
-    debugger; // Native breakpoint
+// To String
+
+//1)
+console.log(typeof(String(null)));
+console.log(typeof(String(4)));
+
+// 2)
+console.log(typeof(5 + ''));
+
+const num = 5;
+console.log("https://www.vk.com/catalog/" + num);
+
+const fontSize = 26 + 'px';
+
+
+// To Number
+
+// 1)
+console.log(typeof(Number('16')));
+
+// 2)
+console.log(typeof(+'5'));
+
+// 3)
+console.log(typeof(parseInt('15px',10)));
+
+let answer = +prompt('Hello, what is your name?', '');
+
+
+// To Boolean
+
+// 0, '', null, undefined, NaN => transforms to false
+// everything else is true
+
+// 1)
+let switcher = null;
+
+if (switcher) {
+    console.log('Working...');
 }
 
-hello();
+switcher = 1;
 
-function hi() {
-    console.log('Say hi!');
+if (switcher) {
+    console.log('Working...');
 }
 
-hi();
+// 2)
+console.log(typeof(Boolean(null)));
 
-const arr = [1, 14, 4, 30, 54],
-      sorted = arr.sort(compareNum);
-
-function compareNum(a, b) {
-    return a - b;
-}
-
-console.log(sorted);
+// 3)
+console.log(typeof(!!'4444'));

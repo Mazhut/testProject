7581f7e52a47b42552525ae84d30777d0255a546
Aliaksei <alxqwerty@gmail.com>
2022-10-13 22:51:14 +0300

    Lesson 029. JS + HTML

--- js/script.js
+++ js/script.js
@@ -1,23 +1,55 @@
 "use strict";
 
-// Old methods
+const box = document.getElementById('box'),
+      btns = document.getElementsByTagName('button'),
+      circles = document.getElementsByClassName('circle'),
+      wrapper = document.querySelector('.wrapper'),
+      hearts = wrapper.querySelectorAll('heart'),
+      oneHeart = wrapper.querySelector('.heart');
 
-const box = document.getElementById('box');
-console.log(box);
+// box.style.backgroundColor = 'blue';
+// box.style.width = '500px';
+// const num = 400;
 
-const btns = document.getElementsByTagName('button')[1];
-console.log(btns);
+// box.style.cssText = `background-color: blue; width: ${num}px`;
+box.style.cssText = `background-color: blue; width: 500px`;
 
-const circles = document.getElementsByClassName('circle');
-console.log(circles);
+btns[1].style.borderRadius = '100%';
+circles[0].style.backgroundColor = 'red';
 
-
-// Current methods
-
-const hearts = document.querySelectorAll('.heart');
-hearts.forEach(item => {
-    console.log(item);
+// for (let i = 0; i < circles.length; i++) {
+//     circles[i].style.backgroundColor = 'purple';
+// }
+hearts.forEach(item => { 
+    item.style.backgroundColor = 'green';
 });
 
-const oneHeart = document.querySelector('.heart');
-console.log(oneHeart);
+const div = document.createElement('div');
+// const text = document.createTextNode('Hello world');
+
+div.classList.add('black');
+
+wrapper.append(div);
+// wrapper.appendChild(div);
+
+// wrapper.prepend(div);
+
+// hearts[0].before(div);
+// hearts[0].after(div);
+
+// wrapper.insertBefore(div, hearts[0]);
+
+// circles[0].remove();
+// wrapper.removeChild(circles[1]);
+
+hearts[0].replaceWith(circles[0]);
+// wrapper.replaceChild(circles[0], hearts[0]);
+
+div.innerHTML = '<h1>Hello world</h1>';
+
+// div.textContent = 'Good night'; //не воспринимает HTML структуру/теги
+
+// div.insertAdjacentHTML("afterend", '<h2>AEW</h2>');
+// div.insertAdjacentHTML("beforebegin", '<h2>AEW</h2>');
+// div.insertAdjacentHTML("afterbegin", '<h2>AEW</h2>');
+// div.insertAdjacentHTML("beforend", '<h2>AEW</h2>');

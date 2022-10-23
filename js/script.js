5bb857989aee79c1dec79550275436fe59c10699
Aliaksei <alxqwerty@gmail.com>
2022-10-23 15:29:14 +0300

    Navigation between DOM elements.

--- js/script.js
+++ js/script.js
@@ -1,53 +1,25 @@
-"use strict";
+// console.log(document.head);
+// console.log(document.documentElement);
+// console.log(document.body.childNodes);
+// console.log(document.body.firstChild);
+// console.log(document.body.firstElementChild);
+// console.log(document.body.lastChild);
+// console.log(document.body.lastElementChild);
 
-const btns = document.querySelectorAll('button');
-const overlay = document.querySelector('.overlay');
+// console.log(document.querySelector('#current').parentNode.parentNode);
+// console.log(document.querySelector('#current').parentElement);
 
-// btn.onclick = function() {
-//     alert('Click');
-// };
+// console.log(document.querySelector("[data-current='3']").nextSibling);
+// console.log(document.querySelector("[data-current='3']").previousSibling);
 
-// btn.onclick = function() {
-//     alert('Second click');
-// };
+// console.log(document.querySelector("[data-current='3']").nextElementSibling);
+// console.log(document.querySelector("[data-current='3']").previousElementSibling);
 
-// const deleteElement = (e) => {
-//     e.target.remove();
-// }; 
+for (let node of document.body.childNodes) {
 
-// btn.addEventListener('mouseenter', (e, ) => {
-//     console.log(e.target);
-//     e.target.remove();
-//     // alert('Element Hovered');
-// });
-
-
-// btn.addEventListener('mouseleave', () => {
-//     alert('Element Unhovered');
-// });
-
-let i = 0;
-
-const viewElement = (e) => {
-    console.log(e.currentTarget);
-    console.log(e.type);
-    // i++;
-    // if (i == 1){
-    //     btn.removeEventListener('click', (viewElement));
-    // }
-};
-
-// btn.addEventListener('click', viewElement);
-// overlay.addEventListener('click', viewElement);
-
-btns.forEach(btn => {
-    btn.addEventListener('click', viewElement, {once: true});
-    });
-
-const link = document.querySelector('a');
-
-link.addEventListener('click', function(event) {
-    event.preventDefault();
-
-    console.log(event.target);
-});
+    if (node.nodeName == '#text'){
+        continue;
+    }
+    
+    console.log(node);
+}

c918d0ee2c7e8bd9c6fbac153a50db1c419f83ba
Aliaksei <alxqwerty@gmail.com>
2022-10-24 22:23:06 +0300

    Defer/Async

--- js/script.js
+++ js/script.js
@@ -1,35 +1,15 @@
-// touchstart — касание к элементу
-// touchmove — палец двигается по элементу
-// touchend — как только касаение прекратилось
-// touchenter — когда ведем пальцом по экрану и нажимаем на элемент на которое повешено событие
-// touchleave — ушел за пределы элемента
-// touchcancel — когда точка соприкосновения больше не регистрируется на поверхности
+'use strict';
 
-window.addEventListener('DOMContentLoaded', () => {
-    const box = document.querySelector('.box');
+const p = document.querySelectorAll('p');
 
-    box.addEventListener('touchstart', (e) => {
-        e.preventDefault();
+console.log(p);
 
-        console.log('Start');
-        console.log(e.targetTouches);
-    });
+function loadScript(src) {
+    const script = document.createElement('script');
+    script.src = src;
+    script.async = false;
+    document.body.append(script);
+}
 
-
-    box.addEventListener('touchmove', (e) => {
-        e.preventDefault();
-
-        console.log(e.targetTouches[0].pageX);
-    });
-
-
-    box.addEventListener('touchend', (e) => {
-        e.preventDefault();
-
-        console.log('End');
-    });
-});
-
-// touches список всех пальцев на экране (неважно, где находятся)
-// targetTouches все пальцы которые взаимодействуют с элементом
-// changedTouches список пальцев, которые участвуют в текущем событии
+loadScript('js/test.js');
+loadScript('js/some.js');

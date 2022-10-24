682e899da70bf60e817884526cf30c57d0620487
Aliaksei <alxqwerty@gmail.com>
2022-10-24 21:44:41 +0300

    Mobile Touches.

--- js/script.js
+++ js/script.js
@@ -1,25 +1,35 @@
-// console.log(document.head);
-// console.log(document.documentElement);
-// console.log(document.body.childNodes);
-// console.log(document.body.firstChild);
-// console.log(document.body.firstElementChild);
-// console.log(document.body.lastChild);
-// console.log(document.body.lastElementChild);
+// touchstart — касание к элементу
+// touchmove — палец двигается по элементу
+// touchend — как только касаение прекратилось
+// touchenter — когда ведем пальцом по экрану и нажимаем на элемент на которое повешено событие
+// touchleave — ушел за пределы элемента
+// touchcancel — когда точка соприкосновения больше не регистрируется на поверхности
 
-// console.log(document.querySelector('#current').parentNode.parentNode);
-// console.log(document.querySelector('#current').parentElement);
+window.addEventListener('DOMContentLoaded', () => {
+    const box = document.querySelector('.box');
 
-// console.log(document.querySelector("[data-current='3']").nextSibling);
-// console.log(document.querySelector("[data-current='3']").previousSibling);
+    box.addEventListener('touchstart', (e) => {
+        e.preventDefault();
 
-// console.log(document.querySelector("[data-current='3']").nextElementSibling);
-// console.log(document.querySelector("[data-current='3']").previousElementSibling);
+        console.log('Start');
+        console.log(e.targetTouches);
+    });
 
-for (let node of document.body.childNodes) {
 
-    if (node.nodeName == '#text'){
-        continue;
-    }
-    
-    console.log(node);
-}
+    box.addEventListener('touchmove', (e) => {
+        e.preventDefault();
+
+        console.log(e.targetTouches[0].pageX);
+    });
+
+
+    box.addEventListener('touchend', (e) => {
+        e.preventDefault();
+
+        console.log('End');
+    });
+});
+
+// touches список всех пальцев на экране (неважно, где находятся)
+// targetTouches все пальцы которые взаимодействуют с элементом
+// changedTouches список пальцев, которые участвуют в текущем событии
